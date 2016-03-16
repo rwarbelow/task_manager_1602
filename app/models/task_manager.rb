11a8a007fe394e12e9f84b3d6238948078c5bab2
Andrew Carmer <carmerandrew@gmail.com>
2016-03-15 21:19:19 -0600

    finish itteration 1

---
+++ app/models/task_manager.rb
@@ -0,0 +1,39 @@
+require 'yaml/store'
+require_relative 'task'
+
+class TaskManager
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(task)
+    database.transaction do
+      database['tasks'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['tasks'] << { "id" => database['total'], "title" => task[:title], "description" => task[:description] }
+    end
+  end
+
+  def raw_tasks
+    database.transaction do
+      database['tasks'] || []
+    end
+  end
+
+  def all
+    raw_tasks.map { |data| Task.new(data) }
+  end
+
+  def raw_task(id)
+    # => The .find method used here is the .find method from Array Class,
+    # => not the find method defined below within TaskManager Class.
+    raw_tasks.find { |task| task["id"] == id }
+  end
+
+  def find(id)
+    Task.new(raw_task(id))
+  end
+end
